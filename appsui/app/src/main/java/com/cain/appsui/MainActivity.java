0abbf398011274babeeea61ddb081dab65a5b2c6
cain007 <cain2009@163.com>
2016-07-24 21:55:31 +0800

    no message

--- appsui/app/src/main/java/com/cain/appsui/MainActivity.java
+++ appsui/app/src/main/java/com/cain/appsui/MainActivity.java
@@ -2,12 +2,34 @@ package com.cain.appsui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        //webView.loadUrl("file:///android_asset/www/test1.html");
+        webView.loadUrl("file:///android_asset/sui03.html");
+
+        //启用支持javascript
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        //webView.loadUrl("http://baidu.com");
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 }
